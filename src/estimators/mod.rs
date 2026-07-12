59cf613e552bf36d756370d34aa96ab4d952559d
alal <lal.apoorva@gmail.com>
2026-07-12 11:11:02 -0700

    Split linear estimator module by domain

--- src/estimators/mod.rs
+++ src/estimators/mod.rs
@@ -1,22 +1,27 @@
 mod balancing;
 mod gmm;
+mod iv;
 mod linear;
 mod mle;
+mod panel;
 mod regularized;
 mod semiparametric;
 mod survival;
+mod synthetic;
 mod transforms;
 
 pub use balancing::BalancingWeights;
 pub use gmm::GMM;
-pub use linear::{
-    av, optimal_g, panel_factor, panel_fe, FixedEffectsOLS, HorizontalPanelRidge,
-    InteractiveFixedEffects, MatrixCompletion, SyntheticControl, SyntheticDID, TwoSLS, OLS,
-};
+pub use iv::TwoSLS;
+pub use linear::{av, optimal_g, FixedEffectsOLS, OLS};
 pub use mle::{Logit, MEstimator, MultinomialLogit, Poisson};
+pub use panel::{
+    panel_factor, panel_fe, HorizontalPanelRidge, InteractiveFixedEffects, MatrixCompletion,
+};
 pub use regularized::{BaggedPolynomialRegressor, ElasticNet, Ridge};
 pub use semiparametric::{AverageDerivative, PartiallyLinearDML, AIPW, EPLM};
 pub use survival::{AndersenGill, CoxPH, ExponentialPH, WeibullPH};
+pub use synthetic::{SyntheticControl, SyntheticDID};
 pub use transforms::{
     KernelBasis, NystromBasis, PcaTransformer, RandomFourierFeatures, RandomizedPcaTransformer,
 };
